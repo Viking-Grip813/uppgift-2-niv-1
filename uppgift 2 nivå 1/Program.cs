93044a2023e0c10b3b3b979b0157c8d48f413ca4
vist07001 <vist07001@E-PW05CGR8>
2024-12-11 12:37:30 +0100

    Add project files.

---
+++ uppgift 2 nivå 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace uppgift2
+{ class Program
+    { static void Main(string[] args)
+        {
+            
+            int tal = 1;
+            while (tal > 0)
+            {
+                Console.WriteLine("Skriv ett tal");
+                int tal1 = int.Parse(Console.ReadLine());
+                if (tal1 == 0) 
+                break;
+
+                
+            }
+        }
+    }
+}
